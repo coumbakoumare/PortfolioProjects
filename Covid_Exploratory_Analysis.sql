e1cfd6437f359672766ce5edd5c86bb4f9714732
Coumba Koumare <106991610+coumbakoumare@users.noreply.github.com>
2022-06-08 23:26:14 -0400

    Update Covid_Exploratory_Analysis.sql

--- Covid_Exploratory_Analysis.sql
+++ Covid_Exploratory_Analysis.sql
@@ -1,53 +1,53 @@
 -- Covid19 Data Exploration - Skills used: Joins, Aggregate Functions, Creating Views
 
-Select Data that I am going to be starting with
+-- Data that I am going to be starting with
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM covid.coviddeaths
-WHERE continent is not null
+WHERE continent IS NOT NULL
 ORDER BY 1,2;
 
 -- Looking at Total Cases vs Total Deaths
 
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
 FROM covid.coviddeaths
-WHERE continent is not null
+WHERE continent IS NOT NULL
 ORDER BY 1,2;
 
 -- Looking at Total Cases vs Population 
 
-SELECT location, date, population, total_cases, (total_cases/population)*100 as case_percentage
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS case_percentage
 FROM covid.coviddeaths
-WHERE continent is not null
+WHERE continent IS NOT NULL
 ORDER BY 1,2;
 
 -- Looking at Countries with Highest Infection Rate Compared to Population 
 
-SELECT location, population, Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as case_percentage
+SELECT location, population, Max(total_cases) AS HighestInfectionCount, Max((total_cases/population))*100 AS case_percentage
 FROM covid.coviddeaths
-WHERE continent is not null
+WHERE continent IS NOT NULL
 GROUP BY continent, population
-ORDER BY case_percentage desc;
+ORDER BY case_percentage DESC;
 
 -- Showing Countries with Highest Death Cunt per Population
 
-SELECT location, Max(total_deaths) as TotalDeathCount
-FROM covid.coviddeaths
-WHERE continent is not null
-GROUP BY continent
-ORDER BY TotalDeathCount desc;
-
--- Showing Continents With the Highest Death Count per Population  
-
-SELECT continent, Max(total_deaths) as TotalDeathCount
+SELECT location, Max(total_deaths) AS TotalDeathCount
 FROM covid.coviddeaths
 WHERE continent IS NOT NULL
 GROUP BY continent
-ORDER BY TotalDeathCount desc;
+ORDER BY TotalDeathCount DESC;
+
+-- Showing Continents With the Highest Death Count per Population  
+
+SELECT continent, Max(total_deaths) AS TotalDeathCount
+FROM covid.coviddeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC;
 
 -- Global Numbers
 
-SELECT date, SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/SUM(new_cases)*100 as deathpercentage
+SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS deathpercentage
 FROM covid.coviddeaths
 WHERE continent IS NOT NULL
 GROUP BY date 
@@ -56,19 +56,19 @@ ORDER BY 1,2;
 -- Looking at Total Population vs Vaccinations 
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
-SUM(vac.new_vaccinations) OVER (Partition by dea.location Order by dea.location, dea.date) as rollingpeoplevaccinated
+SUM(vac.new_vaccinations) OVER (Partition BY dea.location ORDER BY dea.location, dea.date) AS rollingpeoplevaccinated
 From coviddeaths dea
 JOIN covidvaccinations vac
 ON dea.location = vac.location
 AND dea.date = vac.date
-WHERE dea.continent is not null
-ORDER by 2,3;
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3;
 
 -- Creating Views to Store data for later visualizations 
 
 CREATE VIEW TotalPopulationvsVaccinations AS 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
-SUM(vac.new_vaccinations) OVER (Partition by dea.location Order by dea.location, dea.date) as rollingpeoplevaccinated
+SUM(vac.new_vaccinations) OVER (Partition BY dea.location ORDER BY dea.location, dea.date) AS rollingpeoplevaccinated
 FROM coviddeaths dea
 JOIN covidvaccinations vac
 ON dea.location = vac.location
@@ -76,7 +76,7 @@ AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3;
 
-Create View globalnumber AS
+Create VIEW globalnumber AS
 SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS deathpercentage
 FROM covid.coviddeaths
 WHERE continent IS NOT NULL
@@ -84,7 +84,7 @@ GROUP BY date
 ORDER BY  1,2;
 
 CREATE VIEW deathcountpercontinent AS
-SELECT continent, Max(total_deaths) as TotalDeathCount
+SELECT continent, Max(total_deaths) AS TotalDeathCount
 FROM covid.coviddeaths
 WHERE continent IS NOT NULL
 GROUP BY continent
